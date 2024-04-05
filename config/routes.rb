5e24e7e36171053b03694053366f65f1e8205c7d
Mottttton <mottttton.webdev@gmail.com>
2024-04-05 11:29:08 +0900

    modify: rails_adminのインストールと日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
