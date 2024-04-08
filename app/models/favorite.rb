5428b6b199f9f49889868b5d9b2fba72a1e3fdf0
Mottttton <mottttton.webdev@gmail.com>
2024-04-08 16:30:30 +0900

    fix: Modelとエラーメッセージの修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :account
   belongs_to :favoritable, polymorphic: true
+  validates :account_id, uniqueness: { scope: %i(favoritable_id favoritable_type)}
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ApplicationRecord
   belongs_to :account
   belongs_to :taggable, polymorphic: true
+
+  validates :name, presence: true
 end
