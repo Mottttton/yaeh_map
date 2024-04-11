467dc59c0d3046ea40ac58a75503b67f73803068
Mottttton <mottttton.webdev@gmail.com>
2024-04-11 16:53:45 +0900

    modify: お気に入り機能のSystem Spec作成

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,8 @@ class Favorite < ApplicationRecord
   belongs_to :account
   belongs_to :favoritable, polymorphic: true
   validates :account_id, uniqueness: { scope: %i(favoritable_id favoritable_type)}
+
+  scope :search_posts, ->() { find_by(favoritable_type: 'Post') }
+  scope :search_post, ->(post) { find_by(favoritable_type: 'Post', favoritable_id: post.id) }
+  scope :search_account, ->(account) { find_by(account_id: account.id) }
 end
