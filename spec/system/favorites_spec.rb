79d6230750ead5f18e57ff701a8dc7493b9c8ffb
Mottttton <mottttton.webdev@gmail.com>
2024-04-13 22:37:27 +0900

    fix: 投稿詳細画面にお気に入り機能追加

--- spec/system/favorites_spec.rb
+++ spec/system/favorites_spec.rb
@@ -13,24 +13,47 @@ RSpec.describe "Favorites", type: :system do
       click_button('ログイン')
     end
     describe 'お気に入りの登録削除' do
-      it 'お気に入りのつけ外しができる' do
-        visit posts_path
-        first_post = Post.first
-        fav_link_locator = 'fav_link-' + first_post.id.to_s
-        num_of_fav_id = 'num_of_fav-' + first_post.id.to_s
+      context '情報投稿一覧画面' do
+        it 'お気に入りのつけ外しができる' do
+          visit posts_path
+          first_post = Post.first
+          fav_link_locator = 'fav_link-' + first_post.id.to_s
+          num_of_fav_id = 'num_of_fav-' + first_post.id.to_s
 
-        num_of_favs = find_by_id(num_of_fav_id).text
-        expect(num_of_favs).to have_content '0'
+          num_of_favs = find_by_id(num_of_fav_id).text
+          expect(num_of_favs).to have_content '0'
 
-        click_link(fav_link_locator)
-        sleep(0.5)
-        num_of_favs = find_by_id(num_of_fav_id).text
-        expect(num_of_favs).to have_content '1'
+          click_link(fav_link_locator)
+          sleep(0.5)
+          num_of_favs = find_by_id(num_of_fav_id).text
+          expect(num_of_favs).to have_content '1'
 
-        click_link(fav_link_locator)
-        sleep(0.5)
-        num_of_favs = find_by_id(num_of_fav_id).text
-        expect(num_of_favs).to have_content '0'
+          click_link(fav_link_locator)
+          sleep(0.5)
+          num_of_favs = find_by_id(num_of_fav_id).text
+          expect(num_of_favs).to have_content '0'
+        end
+      end
+      context '情報詳細画面' do
+        it 'お気に入りのつけ外しができる' do
+          first_post = Post.first
+          visit post_path(first_post.id)
+          fav_link_locator = 'fav_link-' + first_post.id.to_s
+          num_of_fav_id = 'num_of_fav-' + first_post.id.to_s
+
+          num_of_favs = find_by_id(num_of_fav_id).text
+          expect(num_of_favs).to have_content '0'
+
+          click_link(fav_link_locator)
+          sleep(0.5)
+          num_of_favs = find_by_id(num_of_fav_id).text
+          expect(num_of_favs).to have_content '1'
+
+          click_link(fav_link_locator)
+          sleep(0.5)
+          num_of_favs = find_by_id(num_of_fav_id).text
+          expect(num_of_favs).to have_content '0'
+        end
       end
     end
   end
