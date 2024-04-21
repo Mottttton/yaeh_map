49683922a024bb9c5cdec1b86198d0bfdd85bac8
Mottttton <mottttton.webdev@gmail.com>
2024-04-21 13:40:09 +0900

    modify: トップページ設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: %i(name nickname region self_introduction :portrait))
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    posts_path
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     new_account_session_path
   end
