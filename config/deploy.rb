d4df450ca1700b99e3f4ec0d9440540e33e53fe9
Mottttton <mottttton.webdev@gmail.com>
2024-08-07 10:13:35 +0900

    modify: deploy:cleanupを追加しreleasesを5個に制限&不要なインデント削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,22 +10,23 @@ set :keep_releases, 5
 set :rbenv_ruby, '3.0.1'
 set :log_level, :info
 
+after "deploy:restart", "deploy:cleanup"
 after 'deploy:published', 'deploy:seed'   # 9
- after 'deploy:finished', 'deploy:restart'   # 10
+after 'deploy:finished', 'deploy:restart'   # 10
 
- namespace :deploy do
-   desc 'Run seed'
-   task :seed do
-     on roles(:db) do
-       with rails_env: fetch(:rails_env) do
-         within current_path do
-           execute :bundle, :exec, :rake, 'db:seed'
-         end
-       end
-     end
-   end
-   desc 'Restart application'
-   task :restart do
-     invoke 'unicorn:restart'
-   end
- end
+namespace :deploy do
+  desc 'Run seed'
+  task :seed do
+    on roles(:db) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
